952fd6223f14ff259148bd28c72e1dc1b351ae95
Umar Junaid <Umar Junaid@172.16.102.233>
2019-07-15 11:06:33 +0500

    Lab Terminal

---
+++ calculator1/src/test/java/calculator1/CalculatorTest.java
@@ -0,0 +1,35 @@
+package calculator1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test1() {
+		Calculator c=new Calculator();
+		assertEquals(10,c.equals(20));
+	}
+	@Test
+	public void test2() {
+		
+		Calculator c=new Calculator();
+		assertEquals(10,c.findMax(10, 20, 30));
+	}
+	
+	
+	@Test
+	public void test3() {
+		
+		Calculator c=new Calculator();
+		assertEquals(10,c.equals(30));
+	}
+	
+
+}
